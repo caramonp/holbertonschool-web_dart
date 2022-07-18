538107ac78b31ba356f1549dcbba252ebda14d36
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-07-18 17:20:58 -0500

    assert

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,8 +1,8 @@
 void main(List<String> args) {
-  int number = int.parse(args[0])
+  int nb = int.parse(args[0]);
 
-  assert(number >= 80,
+  assert(nb >= 80,
     'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80'
    );
-   print('You Passed')
+   print('You Passed');
 }
