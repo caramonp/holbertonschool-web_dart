d426add87faf3a9eed9cc861ffb25bb050be632e
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-07-18 17:26:45 -0500

    identify positive or negative number

--- 0x00-hello-dart/6-positive_or_negative.dart
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
   int nb = int.parse(args[0]);
 
-  nb > 0 ? '$nb is positive\n' : nb < 0 ? '$nb is negative\n' : '$nb is zero\n'
+  nb > 0 ? '$nb is positive\n' : nb < 0 ? '$nb is negative\n' : '$nb is zero\n';
 }
