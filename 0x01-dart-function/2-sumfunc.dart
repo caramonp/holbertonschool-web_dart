90a51556de368c94cd8cb8d4bc9caf85e578cff2
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-07-21 17:06:41 -0500

    sum function

--- 0x01-dart-function/2-sumfunc.dart
+++ 0x01-dart-function/2-sumfunc.dart
@@ -1,3 +1,11 @@
+int add(int a, int b){
+  return(a+b);
+}
+
+int sub(int a, int b){
+  return(a-b);
+}
+
 String showFunc(int a, int b) {
   var sum = "Add ${a} + ${b} = ${add(a, b)}\nSub ${a} - ${b} = ${sub(a, b)}";
   return "$sum";
