800251fe924e20d8c3bfba0515647283f1c78351
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-07-22 10:52:29 -0500

    user to json

---
+++ 0x02-dart-classes/1-usertojson.dart
@@ -0,0 +1,13 @@
+class User {
+    String name;
+    int age;
+    double height;
+  
+  User({ required this.name, required this.age, required this.height});
+  
+  Map toJson() => {
+    'name' : name,
+    'age' : age,
+    'height' : height
+   };
+}
