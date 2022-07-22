2cf1504e0f6ee2cd3e81be2b8506f3b294c450b0
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-07-22 16:49:37 -0500

    from json to a class type

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,28 @@
+class User {
+    String name;
+    int age, id;
+    double height;
+
+  
+  User({ required this.name, required this.age, required this.height, required this.id });
+  
+  Map toJson() => {
+     'id': id,
+    'name' : name,
+    'age' : age,
+    'height' : height
+   };
+  
+  static User fromJson(Map<dynamic, dynamic> userJson){
+    final name = userJson['name'];
+    final age = userJson['age'];
+    final id = userJson['id'];
+    final height = userJson['height'];
+    
+    return(User(id: id, name: name, age: age, height: height));
+  }
+  
+   String toString() {
+    return "User(id : $id, name: $name, age: $age, height: $height)";
+  }
+}
