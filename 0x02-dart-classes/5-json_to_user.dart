2f1ad6748e07ed8a091c69a9c7bc9ee6130cf802
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-07-22 16:51:20 -0500

    from json to a class type

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -23,6 +23,6 @@ class User {
   }
   
    String toString() {
-    return "User(id : $id, name: $name, age: $age, height: $height)";
+    return "User(id : $id,name: $name, age: $age, height: $height)";
   }
 }
