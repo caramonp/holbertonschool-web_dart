996aa5ba1380937332c92fb5bf7c577c25a9a882
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-07-22 11:28:59 -0500

    validated a password

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,21 @@
+class Password {
+    String password = '';
+
+    bool isValid() {
+     
+      if(password.length >= 8 || password.length <= 16){
+    return false;
+  }
+      
+  bool hasUppercase = password.contains(new RegExp(r'[A-Z]'));
+  bool hasDigits = password.contains(new RegExp(r'[0-9]'));
+  bool hasLowercase = password.contains(new RegExp(r'[a-z]'));
+      
+return hasDigits & hasUppercase & hasLowercase;
+
+  }
+  
+ String toString() {
+    return "Your Password is: $password";
+  }
+}
