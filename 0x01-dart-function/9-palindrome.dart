3568ed4b7828fed5f78a087123b09b6bedb922c9
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-07-21 20:52:00 -0500

    last task

---
+++ 0x01-dart-function/9-palindrome.dart
@@ -0,0 +1,9 @@
+bool isPalindrome(String s) {
+  if (s.length < 3) return false;
+  String? reverse = s.split('').reversed.join('');
+  if (s == reverse) {
+    return true;
+  } else {
+    return false;
+  }
+}
